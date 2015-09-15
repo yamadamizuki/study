41fdacbcf3015bb8536b4d826119b604d77c7019
mizuki <yamada@nadia.bz>
2015-09-16 08:50:28 +0900

    資料作成

--- demo/jquery.yamada.js
+++ demo/jquery.yamada.js
@@ -1,5 +1,23 @@
-jQuery.fn.yamada = function(options){
-	return this.each(function(){
-		var dts = (this).children('dt');
-	});
-}
+(function($){
+	$.fn.yamada = function(options){
+		var settings = $.extention({},{open:false},options);
+		return this.each(function(){
+			var dts = (this).children('dt');
+			dts.click(onClick);
+			dts.each(reset);
+			if(settings.open) $(this).children('dt:first-child').next().show;
+		});
+
+		function onClick() {
+			$(this).siblings('dt').each(hide);
+			$(this).next().slideDown('fast');
+			return false;
+		}
+		function hide() {
+			$(this).next().slideUp('fast');
+		}
+		function reset(){
+			$(this).next().hide();
+		}
+	}
+})(jQuery);
